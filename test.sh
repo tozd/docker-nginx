9767ab0fd3de90ddb526b5d08b57ae8bf4e45f3a
Mitar <mitar.git@tnode.com>
2023-05-06 23:03:57 +0200

    Add test.

---
+++ test.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -e
+
+cleanup_docker=0
+cleanup_image=0
+cleanup() {
+  if [ "$cleanup_docker" -ne 0 ]; then
+    echo "Stopping Docker image"
+    docker stop test
+  fi
+
+
+  if [ "$cleanup_image" -ne 0 ]; then
+    echo "Removing Docker image"
+    docker image rm testimage
+  fi
+}
+
+trap cleanup EXIT
+
+echo "Building Docker image"
+time docker build -t testimage -f test/Dockerfile --build-arg "IMAGE=${CI_REGISTRY_IMAGE}:${TAG}" test
+cleanup_image=1
+
+echo "Running Docker image"
+docker run -d --name test --rm -p 80:80 testimage
+cleanup_docker=1
+
+echo "Sleeping"
+sleep 5
+
+echo "Testing"
+ADDRESS="$(LC_ALL=C nslookup -type=a docker 2>/dev/null | sed -nr '/Name/,+1s|Address(es)?: *||p')"
+echo "$ADDRESS site.test" >> /etc/hosts
+wget -T 30 -q -O - http://site.test:80 | grep -q '<title>Test site</title>'
